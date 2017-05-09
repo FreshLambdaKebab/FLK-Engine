31e9f934653cc93008a7906f4a1988952d785291
Duranskinho <duranski17@gmail.com>
2017-05-10 09:12:28 +1200

    Drawing simple polygons
    yes yes its all in one main file but that will soon cease.

--- FLKEngine/src/main.cpp
+++ FLKEngine/src/main.cpp
@@ -2,6 +2,34 @@
 #include <SDL\SDL.h>
 #include <SDL\SDL_opengl.h>
 #include <stdio.h>
+#include <string>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const std::string WINDOW_TITLE = "God damn fucking window";
+
+//shaders
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    in vec3 color;
+    out vec3 Color;
+    void main()
+    {
+        Color = color;
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+    in vec3 Color;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(Color, 1.0);
+    }
+)glsl";
 
 int main(int argc,char* argv[])
 {
@@ -14,24 +42,78 @@ int main(int argc,char* argv[])
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
 	//create sdl window
-	SDL_Window* window = SDL_CreateWindow("God damn fucking window", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH,SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 	
 	//initialize glew
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	//testing
-	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer);
+	//create a vertex array object
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	
+	//create a vertex buffer object and copy the data to it
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
 
-	printf("%u\n", vertexBuffer);
+	//setup vertices for triangle
+	GLfloat vertices[] = {
+		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+	};
+
+	//upload and copy the vertex data 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//create an element array
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	
+	GLuint elements[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+	//create and compile the shaders
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//link the vertex and frag shaders into a shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	//specify the layout of the vertex data
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+
+	GLint colorAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colorAttrib);
+	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
 
 	SDL_Event windowEvent;
 	bool quit = false;
 	//main loop
 	while (!quit)
-	{
+	{	
 		while(SDL_PollEvent(&windowEvent))
 		{
 			switch (windowEvent.type)
@@ -51,9 +133,24 @@ int main(int argc,char* argv[])
 		}
 
 		//do shit
+		//clear the screen to a desired color
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//draw the triangle from the 3 vertices
+		glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT,0);
 		
 		SDL_GL_SwapWindow(window);
 	}
+
+	//delete arrays buffers and shaders
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteVertexArrays(1, &VAO);
 
 	SDL_GL_DeleteContext(context);
 	SDL_Quit();
