07bbf13341c4350e7b7445091c72d5e39adcc29e
Duranskinho <duranski17@gmail.com>
2017-05-18 09:11:38 +1200

    Rendering a 3D cube!
    fuck that boring ass 2D rendering lets get 3D up in this bitch

--- FLKEngine/res/shaders/colorShader.vert
+++ FLKEngine/res/shaders/colorShader.vert
@@ -7,11 +7,13 @@ in vec2 texcoord;
 out vec3 Color;
 out vec2 Texcoord;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
 	Color = color;
 	Texcoord = vec2(texcoord.x,1.0 - texcoord.y);
-	gl_Position = transform * vec4(position, 1.0);
+	gl_Position = projection * view * model * vec4(position, 1.0);
 }
--- FLKEngine/src/main.cpp
+++ FLKEngine/src/main.cpp
@@ -4,11 +4,9 @@
 #include <SOIL\SOIL.h>
 #include <glm\glm.hpp>
 #include <glm\gtc\matrix_transform.hpp>
-#include <glm\gtc\type_ptr.hpp>
 
 #include "Shader.h"
 #include "Texture2D.h"
-#include "Sprite.h"
 
 //constants
 const int SCREEN_WIDTH = 800; 
@@ -32,6 +30,7 @@ int main(int argc,char* argv[])
 	//initialize glew
 	glewExperimental = GL_TRUE;
 	glewInit();
+	glEnable(GL_DEPTH_TEST);
 
 	//create a vertex array object
 	GLuint VAO;
@@ -44,11 +43,47 @@ int main(int argc,char* argv[])
 
 	//setup vertices for triangle
 	GLfloat vertices[] = {
-		// Positions          // Colors           // Texture Coords
-		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left 
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 	//upload and copy the vertex data 
@@ -70,33 +105,23 @@ int main(int argc,char* argv[])
 	//create and compile the shaders
 	Shader colorShader;
 	colorShader.Load("res/shaders/colorShader.vert", "res/shaders/colorShader.frag");
-	Shader spriteShader;
-	spriteShader.Load("res/shaders/spriteShader.vert", "res/shaders/spriteShader.frag");
-	// Configure shaders
-	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(SCREEN_WIDTH),
-		static_cast<GLfloat>(SCREEN_HEIGHT), 0.0f, -1.0f, 1.0f);
-	spriteShader.SetInteger("image", 0);
-	spriteShader.SetMatrix4("projection", projection,GL_TRUE);
 
 	//specify the layout of the vertex data
 	GLint posAttrib = glGetAttribLocation(colorShader.GetProgramID(), "position");
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 
 	GLint colorAttrib = glGetAttribLocation(colorShader.GetProgramID(), "color");
 	glEnableVertexAttribArray(colorAttrib);
-	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
 	GLint texAttrib = glGetAttribLocation(colorShader.GetProgramID(), "texcoord");
 	glEnableVertexAttribArray(texAttrib);
-	glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+	glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
 	//load texture
 	Texture2D texture;
-	texture.Load("res/textures/awesomeface.png", GL_TRUE);
-	
-	//create sprite
-	Sprite sprite(spriteShader);
+	texture.Load("res/textures/awesomeface.png",GL_FALSE);
 
 	SDL_Event windowEvent;
 	bool quit = false;
@@ -123,17 +148,27 @@ int main(int argc,char* argv[])
 
 		//do shit
 		//clear the screen to a desired color
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//bind teh texture
-		//texture.Bind();
+		texture.Bind();
+		colorShader.Use();
 
-		//colorShader.Use();
-		sprite.Draw(texture, glm::vec2(100, 100),glm::vec2(100,100));
+		//create transformations
+		glm::mat4 model;
+		glm::mat4 view;
+		glm::mat4 projection;
+		model = glm::rotate(model, glm::radians(-50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f,0.0f,-3.0f));
+		projection = glm::perspective(glm::radians(45.0f), static_cast<GLfloat>(SCREEN_WIDTH) / static_cast<GLfloat>(SCREEN_HEIGHT), 0.1f, 100.0f);
+		//get the uniform locations & pass them to the shader
+		colorShader.SetMatrix4("model", model, GL_TRUE);
+		colorShader.SetMatrix4("view", view, GL_TRUE);
+		colorShader.SetMatrix4("projection", projection, GL_TRUE);
 
 		//draw the triangle from the 3 vertices
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		SDL_GL_SwapWindow(window);
 	}
